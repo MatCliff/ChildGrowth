a208f70e4dd6b74435b9ebede93e482f6ebed316
stanleylexander <stanley.6ho@gmail.com>
2025-10-20 21:55:47 +0700

    profile done

--- app/src/main/java/com/ubaya/childgrowth/view/ProfileFragment.kt
+++ app/src/main/java/com/ubaya/childgrowth/view/ProfileFragment.kt
@@ -1,9 +1,11 @@
 package com.ubaya.childgrowth.view
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.ubaya.childgrowth.databinding.FragmentProfileBinding
 
@@ -17,5 +19,37 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val sharePref = requireActivity().getSharedPreferences("profile_data", Context.MODE_PRIVATE)
+
+        val name = sharePref.getString("name", "Udin Dindin")
+        val birthdate = sharePref.getString("birthdate", "01/05/2004")
+        val gender = sharePref.getString("gender", "Male")
+
+        binding.txtInputName.setText(name)
+        binding.txtInputBirthdate.setText(birthdate)
+        if(gender == "Male"){
+            binding.radioBtnMale.isChecked = true
+        }else{
+            binding.radioBtnFemale.isChecked = true
+        }
+
+        binding.btnSaveProfile.setOnClickListener{
+            val inputName = binding.txtInputName.text.toString()
+            val inputBirthdate = binding.txtInputBirthdate.text.toString()
+            val inputGender = if(binding.radioBtnMale.isChecked) "Male" else "Female"
+
+            if(inputName.isEmpty() || inputBirthdate.isEmpty() || inputGender.isEmpty()){
+                Toast.makeText(requireContext(), "All fields must be filled", Toast.LENGTH_SHORT).show()
+            }
+
+            sharePref.edit().apply{
+                putString("name", inputName)
+                putString("birthdate", inputBirthdate)
+                putString("gender", inputGender)
+            }
+
+            Toast.makeText(requireContext(), "Profile saved", Toast.LENGTH_SHORT).show()
+        }
     }
 }
